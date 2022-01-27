17fab2e6b5526989fabff80b987500b99ec52f8d
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-01-26 19:30:57 -0500

    19:30

--- app/src/main/java/com/nadershamma/apps/androidfunwithflags/LoginActivityMEBQ.java
+++ app/src/main/java/com/nadershamma/apps/androidfunwithflags/LoginActivityMEBQ.java
@@ -2,12 +2,40 @@ package com.nadershamma.apps.androidfunwithflags;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivityMEBQ extends AppCompatActivity {
+
+    private EditText editText_usuario;
+    private EditText editText_contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editText_usuario = findViewById(R.id.editText_usuario);
+        editText_contraseña = findViewById(R.id.editText_contraseña);
     }
+
+    public void Login(View view){
+        String usuarioLogin = editText_usuario.getText().toString();
+        String contraseña = editText_contraseña.getText().toString();
+
+        if(usuarioLogin.matches("") || contraseña.matches("")){
+
+            Toast.makeText(this, "Ingresar usuario y contraseña.", Toast.LENGTH_SHORT).show();
+            return;
+        }else {
+            if (usuarioLogin.matches("usuario1") && contraseña.matches("1234")) {
+                Toast.makeText(this, "Login exitoso.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Contraseña incorrecta.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 }
